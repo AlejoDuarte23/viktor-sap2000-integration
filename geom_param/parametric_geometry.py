d95cbbcd8ae5f4975a2b1aefb1ca1e792f75ac70
unknown <alejandro7899871776@gmail.com>
2024-09-02 20:56:59 -0500

    refactor: fix parametric model for even diagonal number

--- geom_param/parametric_geometry.py
+++ geom_param/parametric_geometry.py
@@ -26,22 +26,20 @@ class TowerColumn():
 
     def connect_stringers(self, stringer_1_ids:List, stringer_2_ids:List)-> None:
         if self.n_diagonals % 2 ==0:
-            required_st1_ids = stringer_1_ids[:-1:2]  
+            # Shortest way to get the right nodes
+
+            required_st1_ids = stringer_1_ids[::2]  
             required_st2_ids = stringer_2_ids[1:-1:2]
 
-            print(required_st1_ids)
-            print(required_st2_ids)
+            tags_st1 = sorted([required_st1_ids[0]] + [required_st1_ids[-1]] + required_st1_ids[1:-1] * 2)
+            tags_st2 = sorted( required_st2_ids[:] * 2)
 
-            for i in range(self.n_diagonals):
-                if i ==0:
-                    self.lines[self.current_lines_id] = {"start":required_st1_ids[i], "end":required_st2_ids[i]}
-                elif  i % 2 == 0:
-                    self.lines[self.current_lines_id] = {"start":required_st1_ids[i-1], "end":required_st2_ids[i-1]}
-                else:
-                    self.lines[self.current_lines_id] = {"start":required_st1_ids[i-1], "end":required_st2_ids[i-2]}
-                print("connect_stringers", i)
-                print(self.lines[self.current_lines_id])
+
+
+            for tag_st1, tag_st2 in zip(tags_st1, tags_st2):    
+                self.lines[self.current_lines_id] = {"start":tag_st1, "end": tag_st2}
                 self.current_lines_id += 1
+
         else:
             
             # Shortest way to get the right nodes
